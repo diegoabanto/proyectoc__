82d627e17bfed0532e887514a577f386d86154e8
diegoabanto <diego.abanto@utec.edu.pe>
2019-10-25 16:22:15 -0500

    fourth commit

---
+++ CProducto.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Asus on 25/10/2019.
+//
+
+#include "CProducto.h"
---
+++ CProducto.h
@@ -0,0 +1,14 @@
+//
+// Created by Asus on 25/10/2019.
+//
+
+#ifndef PROYECTOC___CPRODUCTO_H
+#define PROYECTOC___CPRODUCTO_H
+
+
+class CProducto {
+
+};
+
+
+#endif //PROYECTOC___CPRODUCTO_H
