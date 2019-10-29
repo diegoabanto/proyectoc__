c9e5c275556e926decb4c221d20d97910fd2659b
lesly2002 <50913799+lesly2002@users.noreply.github.com>
2019-10-28 21:49:45 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include<vector>
 int main() {
     positivo numero_robots;
-    vector<vector<positivo>> posiciones_origen;
+    vector<vector<positivo> > posiciones_origen;
     vector<texto> datos= lectura(); /*
  * Lee los archivos y guarda cada linea en el vector.por ejemplo: la linea 1 del texto vendria ser vector[0
  */
