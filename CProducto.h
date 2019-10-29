9067d77bb6c100fe192166033b8913a4d83a0248
lesly2002 <50913799+lesly2002@users.noreply.github.com>
2019-10-28 22:08:07 -0500

    Update CProducto.h

--- CProducto.h
+++ CProducto.h
@@ -1,6 +1,10 @@
 #ifndef PROYECTOC___CPRODUCTO_H
 #define PROYECTOC___CPRODUCTO_H
 class CProducto {
+  nombre nombre_producto;
+  nombre tipo_producto;
+  nombre tamaño_producto;
+  nombre peso_producto;
 
 };
 
