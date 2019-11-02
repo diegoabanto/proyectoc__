86a06248b989914fdb706a5d8526e86eb0308863
lesly2002 <50913799+lesly2002@users.noreply.github.com>
2019-11-01 23:13:20 -0500

    Update CRobot.cpp

--- CRobot.cpp
+++ CRobot.cpp
@@ -5,16 +5,15 @@ CRobot::CRobot() {
 }
 
 CRobot::CRobot(positivo num, vector<positivo> begin, vector<positivo> finish, texto product, texto operation) {
-
-    numero = num;
-    origen = begin;
-    destino = finish;
-    producto = product;
-    operacion = operation;
+  
+    numero = num ;
+    origen = begin ;
+    destino = finish ;
+    producto = product ;
+    operacion = operation ;
 }
 
 CRobot::CRobot(CRobot &robot) {
-  
     numero = robot.numero;
     origen = robot.origen;
     destino = robot.destino;
@@ -24,11 +23,12 @@ CRobot::CRobot(CRobot &robot) {
 }
 
 normal CRobot::ejercutarRuta() {
-    cout<<"falta llenar";
+  
 }
 
-normal CRobot::MoverseVerticalmente(entero fila,entero columnas) {
-    cout<<"falta llenar";
+normal CRobot::MoverseVerticalmente() {
+ 
+
 }
 
 normal CRobot::MoverseHorizontalmente(){
@@ -43,14 +43,6 @@ normal CRobot::RegresarHome(){
 
 }
 
-positivo CRobot:: get_numero(){
-      return numero;}
-vector<positivo> get_origen(){
-      return origen;}
-vector<positivo> get_destino(){
-      return destino;}
-texto CRobot:: get_producto(){
-      return producto;}
-texto CRobot:: get_operacion(){ 
-      return operacion;}
+
+
 
