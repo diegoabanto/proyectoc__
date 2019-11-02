f39f38c62d7826c625ed4f12d3bbff2c13167d9a
lesly2002 <50913799+lesly2002@users.noreply.github.com>
2019-11-01 19:15:45 -0500

    Update CRobot.cpp

--- CRobot.cpp
+++ CRobot.cpp
@@ -19,6 +19,27 @@ CRobot::CRobot(positivo num, vector<positivo> begin, vector<positivo> finish, te
 CRobot::CRobot(CRobot &robot) {
     //no seria lo mismo?
     numero = robot.numero;
+    #include "CRobot.h"
+#include "funciones.h"
+
+CRobot::CRobot() {
+    numero = 0;
+    origen = {0,0};
+    destino = {0,0};
+}
+
+CRobot::CRobot(positivo num, vector<positivo> begin, vector<positivo> finish, texto product, texto operation) {
+    //no seria lo mismo?
+    numero = num;
+    origen = begin;
+    destino = finish;
+    producto = product;
+    operacion = operation;
+}
+
+CRobot::CRobot(CRobot &robot) {
+    //no seria lo mismo?
+    numero = robot.numero;
     origen = robot.origen;
     destino = robot.destino;
     producto = robot.producto;
@@ -46,15 +67,5 @@ normal CRobot::RegresarHome(){
 
 }
 
-positivo CRobot::get_numero(){
-  return numero;
-}
 
-vector<positivo> get_origen(){
-      return origen;};
-vector<positivo> get_destino(){
-  return destino;};
-texto get_producto(){
-      return producto;};
-texto get_operacion(){ 
-      return operacion;};
+
