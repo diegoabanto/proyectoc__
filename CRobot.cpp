9c1ef11d878d9247a35bac4a6989f5c6ff90ca22
lesly2002 <50913799+lesly2002@users.noreply.github.com>
2019-11-22 16:48:31 -0500

    Update CRobot.cpp

--- CRobot.cpp
+++ CRobot.cpp
@@ -1,28 +1,46 @@
   
+ 
 #include "CRobot.h"
 #include "CSlot.h"
+
 CRobot::CRobot(positivo& num) {
     numero = num;
+
 }
 
 CRobot::CRobot(vector<positivo> &begin) {
     origen = begin;
 }
 
-normal CRobot::MoverseVerticalmenteArriba(CSlot slot_llegada, CAlmacen almacen, CSlot){
+normal CRobot::MoverseVerticalmenteArriba(CSlot m_slot_llegada, CAlmacen m_almacen, CSlot){
+
 
 }
 
-normal CRobot::MoverseVerticalmenteAbajo(CSlot slot_llegada, CAlmacen almacen, CSlot){
-
+normal CRobot::MoverseVerticalmenteAbajo(CSlot m_slot_llegada, CAlmacen m_almacen, CSlot){
+    slot_llegada=m_slot_llegada;
+    almacen = m_almacen;
 }
 
-normal CRobot::MoverseHorizontalmenteDerecha(CSlot slot_llegada, CAlmacen almacen, CSlot){
-
+normal CRobot::MoverseHorizontalmenteDerecha(CSlot m_slot_llegada, CAlmacen m_almacen, CSlot){
+    slot_llegada=m_slot_llegada;
+    almacen=m_almacen;
 }
 
-normal CRobot::MoverseHorizontalmenteIzquierda(CSlot slot_llegada, CAlmacen almacen, CSlot){
-  
+normal CRobot::MoverseHorizontalmenteIzquierda(CSlot m_slot_llegada, CAlmacen m_almacen, CSlot){
+  slot_llegada=m_slot_llegada;
+  almacen=m_almacen;
 }
 
+normal CRobot::IngresarProducto() {
+}
 
+normal CRobot::RecogerProducto(CSlot m_slot_llegada, CAlmacen m_almacen, CSlot) {
+    slot_llegada=m_slot_llegada;
+    almacen=m_almacen;
+}
+
+normal CRobot::RegresarHome(CSlot m_slot_llegada, CAlmacen m_almacen, CSlot) {
+    slot_llegada=m_slot_llegada;
+    almacen=m_almacen;
+}
