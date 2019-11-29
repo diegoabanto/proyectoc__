e48f6e3cb460a5c2f897582fd66ecede4c4a8394
diegoabanto <diego.abanto@utec.edu.pe>
2019-11-29 02:40:29 -0500

    eight commit

--- funciones.h
+++ funciones.h
@@ -6,12 +6,9 @@
 #include <string>
 #include <vector>
 #include <fstream>
-#include <cstdlib>
 using namespace std;
-typedef size_t range;
 typedef int entero;
 typedef double decimal;
-typedef size_t positivo;
 typedef void normal;
 typedef string texto;
 typedef bool booleano;
